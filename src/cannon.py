9846512061d2dda3515cd51b21c7d50678299850
poorvapisal <82924157+poorvapisal@users.noreply.github.com>
2022-10-07 12:07:16 +0530

    Create cannon.py

---
+++ src/cannon.py
@@ -0,0 +1,15 @@
+from src.headers import *
+from src.building import *
+
+class Cannon(Building):
+
+    def __init__(self, x, y):
+        Building.__init__(self, x, y, 1, 5)
+
+    def cannon_show(self, grid):
+        x = self.getx()
+        y = self.gety()
+
+        grid[y][x] = Fore.YELLOW + "$" + Fore.RESET   
+        
+           
